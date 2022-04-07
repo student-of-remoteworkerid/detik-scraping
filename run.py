e06afeb967bb32dd13f64f3fec69fcb41224be60
Muhammad Shaufi Imanulhaq <shaufiman@gmail.com>
2022-04-07 08:24:46 +0800

    add file run and folder templates

---
+++ run.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/detik-popular')
+def detik_popular():
+    url = requests.get('https://www.detik.com/terpopuler', params={'tag_from': 'wp_cb_mostPopular_more'})
+
+    soup = BeautifulSoup(url.text, 'html.parser')
+
+    popular_area = soup.find(attrs={'class': 'grid-row list-content'})
+
+    titles = popular_area.find_all(attrs={'class': 'media__title'})
+    images = popular_area.find_all(attrs={'class': 'media__image'})
+
+    return render_template('index.html', images=images)
+
+if __name__ == '__main__':
+    app.run(debug=True)
